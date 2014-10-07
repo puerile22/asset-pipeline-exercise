ab6585fd41bec74f3a272aa6180851a5c06aceb5
Peng Gao <puerile22@gmail.com>
2014-10-07 13:22:29 -0700

    assets pipeline exercise

--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -1,4 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-console.log('I\'m the home page!');
+$('document').ready(function() {
+  if ($('body.home_page').length) {
+    console.log('I\'m the home page!');
+
+    //... video js code here
+  }
+});
